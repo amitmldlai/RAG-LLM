419ae28cf4ff6628d400ae036bec1083cb0dab46
amitvkyc <amityadav@idrbt.ac.in>
2025-01-13 12:57:23 +0530

    13Jan

--- YouTube-RAG/create_index.py
+++ YouTube-RAG/create_index.py
@@ -1,41 +1,69 @@
-from llama_index.core.indices import MultiModalVectorStoreIndex
 from llama_index.core import VectorStoreIndex
-import pickle
+from llama_index.multi_modal_llms.gemini import GeminiMultiModal
+from llama_index.core.schema import ImageDocument
+from llama_index.core.schema import TextNode
 import os
-from llama_parse import LlamaParse
-from dotenv import load_dotenv
-from llama_index.core import StorageContext
 import glob
-from llama_index.vector_stores.lancedb import LanceDBVectorStore
+from dotenv import load_dotenv
+from llama_index.core import StorageContext, load_index_from_storage
+from llama_index.core import Settings
+from llama_index.embeddings.google import GooglePaLMEmbedding
+from llama_index.llms.gemini import Gemini
+
+
 load_dotenv()
 
 cwd = os.getcwd()
 data_path = os.path.join(cwd, "data")
-compiled_data = glob.glob(data_path+'/audio/*.mp3') + glob.glob(data_path+'/image/*.png') + glob.glob(data_path+'/text/*.txt')
+embed_model = GooglePaLMEmbedding(model_name="models/embedding-gecko-001")
+llm = Gemini(model="models/gemini-1.5-flash")
+Settings.embed_model = embed_model
+Settings.model = llm
 
 
-def load_or_parse_data():
-    data_file = os.path.join(cwd, "parsed_data.pkl")
+def create_image_text_node():
+    image_text_node = list()
+    llm_vision = GeminiMultiModal(model_name='models/gemini-1.5-flash')
+    image_path = glob.glob(data_path + "/image/" + "*.png")
 
-    if os.path.exists(data_file):
-        with open(data_file, "rb") as f:
-            parsed_data = pickle.load(f)
-    else:
-        parser = LlamaParse(api_key=os.getenv('LLAMA_CLOUD_API_KEY'), result_type='markdown')
-        parsed_data = parser.load_data(file_path=compiled_data)
-        with open(data_file, "wb") as f:
-            pickle.dump(llama_parse_document, f)
+    image_documents = [ImageDocument(image_path=path) for path in image_path]
+    for image in image_documents:
+        resp = llm_vision.complete(
+            prompt="Summarize the image ",
+            image_documents=[image],
+        )
 
-    return parsed_data
-
-
-text_store = LanceDBVectorStore(uri="lancedb", table_name="text_collection")
-image_store = LanceDBVectorStore(uri="lancedb", table_name="image_collection")
-storage_context = StorageContext.from_defaults(vector_store=text_store, image_store=image_store)
+        text_node = TextNode(
+            text=resp.text,
+            metadata={"image_path": image.image_path}
+        )
+        image_text_node.append(text_node)
+    return image_text_node
 
 
-llama_parse_document = load_or_parse_data()
-index = MultiModalVectorStoreIndex.from_documents(
-    llama_parse_document,
-    storage_context=storage_context,
-)
+def create_audio_text_node():
+    text_path = glob.glob(data_path + "/text/*.txt")[0]
+    with open(text_path, "r") as file:
+        text = file.read()
+    audio_text_node = TextNode(
+        text=text,
+        metadata={"audio_path": text_path}
+    )
+    return audio_text_node
+
+
+if os.path.exists('./storage'):
+    storage_context = StorageContext.from_defaults(persist_dir='./storage')
+    index = load_index_from_storage(storage_context)
+else:
+    image_node = create_image_text_node()
+    audio_node = create_audio_text_node()
+    index = VectorStoreIndex(image_node+[audio_node])
+    index.storage_context.persist(persist_dir="./storage")
+
+query_engine = index.as_query_engine()
+
+while True:
+    query = input("Please add your query:")
+    response = query_engine.query(query)
+    print(response)
